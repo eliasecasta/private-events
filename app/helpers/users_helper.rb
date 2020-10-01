6a3bb6777d60a39b66aa9e29b6db2ce423aaf131
Elias Castañeda <e71az@icloud.com>
2020-10-01 11:04:55 -0600

    Added upcoming and past scopes to model, created navigation

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,19 +8,19 @@ module UsersHelper
     current_user ? current_user.events : nil
   end
 
-  def events_grouped_by_date
-    return unless list_events
+  # def events_grouped_by_date
+  #   return unless list_events
 
-    current_date = Time.now
-    list_events
-      .reject { |event| event.date.nil? }
-      .reduce({ upcoming: [], previous: [] }) do |events, event|
-      if event.date >= current_date
-        events[:upcoming] = events[:upcoming] << event
-      else
-        events[:previous] = events[:previous] << event
-      end
-      events
-    end
-  end
+  #   current_date = Time.now
+  #   list_events
+  #     .reject { |event| event.date.nil? }
+  #     .reduce({ upcoming: [], previous: [] }) do |events, event|
+  #     if event.date >= current_date
+  #       events[:upcoming] = events[:upcoming] << event
+  #     else
+  #       events[:previous] = events[:previous] << event
+  #     end
+  #     events
+  #   end
+  # end
 end
